868a1c1319c11b9cb391ac4a6c782819b687fb0f
alexandernovadev <alexsk88.dev@gmail.com>
2024-10-27 20:35:02 -0500

    Refactor DetailsScreen component and remove unnecessary comments

--- app/Details.tsx
+++ app/Details.tsx
@@ -2,7 +2,7 @@ import React, { useRef } from "react";
 import { View, StyleSheet, ScrollView, Text } from "react-native";
 import * as Speech from "expo-speech";
 import Markdown from "react-native-markdown-display";
-import { useLocalSearchParams } from "expo-router"; // Import useLocalSearchParams
+import { useLocalSearchParams } from "expo-router";
 import { markdownText } from "@/data/markdown";
 
 const DOUBLE_PRESS_DELAY = 300;
@@ -15,7 +15,7 @@ export default function DetailsScreen() {
   // Función para manejar el clic de cada palabra
   const speakWord = (word: string) => {
     Speech.speak(word, {
-      language: "en-US", // Especifica el idioma inglés (Estados Unidos)
+      language: "en-US",
     });
   };
 
@@ -27,11 +27,10 @@ export default function DetailsScreen() {
       <Text style={textStyles}>
         {words.map((word, index) => (
           <Text
-            key={index + word}
+            key={`word_${index}`} // Clave única
             onPress={() => {
               const now = Date.now();
               if (now - lastTap.current < DOUBLE_PRESS_DELAY) {
-                // Doble toque detectado
                 speakWord(word);
               }
               lastTap.current = now;
@@ -59,7 +58,6 @@ export default function DetailsScreen() {
       const content = node.children[0].children[0].content || "";
       return renderWords(content, styles.heading2);
     },
-    // Puedes agregar más reglas para heading3, heading4, etc., si lo necesitas
   };
 
   return (
@@ -92,7 +90,6 @@ const styles = StyleSheet.create({
     flex: 1,
     width: "100%",
   },
-  // Markdown text styles
   heading1: {
     fontSize: 28,
     color: "white",
