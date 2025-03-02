283fb436beccd68668f4e3574de19de9a2604c44
alexandernovadev <alexsk88.dev@gmail.com>
2025-03-02 10:14:25 -0500

    Update dependencies and change BACKURL for API integration

--- api/backurl.ts
+++ api/backurl.ts
@@ -1,1 +1,2 @@
-export const BACKURL = "https://www.alexandernova.pro";
+// export const BACKURL = "https://www.alexandernova.pro";
+export const BACKURL = "https://languages-ai-back.alexandernova.pro"
